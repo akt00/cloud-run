184a2232f42c5a6ac8342f62165147dc28c3356a
Aki <84285825+akt00@users.noreply.github.com>
2024-12-19 18:13:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,4 @@ def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    # This block is NOT needed for Cloud Run
-    # port = int(os.environ.get("PORT", 8080))
-    # app.run(debug=True, host='0.0.0.0', port=port)
     pass # Remove the app.run line
